eebd475185a0b3091fa1220f5659c6b1e55d0e96
AnastasiiaEssina <145222942+AnastasiiaEssina@users.noreply.github.com>
2023-12-08 16:34:26 +0600

    Add files via upload

---
+++ Алгоритмизация_и_программирование/Дороги и мосты.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Дороги_и_мосты
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите количество интервалов");
+            int N = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите длину моста");
+            int t = Convert.ToInt32(Console.ReadLine());
+            int result = 0;
+            int sumB = 0 , sumA = 0;
+            Console.WriteLine("Введите пары чисел");
+            for (int i = 0; i < N; i++)
+            {
+                int a = Convert.ToInt32(Console.ReadLine());
+                int b = Convert.ToInt32(Console.ReadLine());
+                sumA += a;
+                sumB += b;
+                result = Math.Min(sumA + t, result + b);
+            }
+            if (result == sumB)
+                result += t;
+            Console.WriteLine(result);
+            Console.ReadKey();
+        }
+    }
+}
