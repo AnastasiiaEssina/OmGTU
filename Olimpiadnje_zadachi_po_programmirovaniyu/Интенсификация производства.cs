c11a0d2d9dd48f9f63caaed9e8192d9e69e4bad1
AnastasiiaEssina <145222942+AnastasiiaEssina@users.noreply.github.com>
2023-12-17 22:13:43 +0600

    Add files via upload

---
+++ Olimpiadnje_zadachi_po_programmirovaniyu/Интенсификация производства.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Интенсификация_производства
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime a = DateTime.Parse(Console.ReadLine());
+            DateTime b = DateTime.Parse(Console.ReadLine());
+            int p = int.Parse(Console.ReadLine());
+            double r = (b - a).TotalDays + 1;
+            r = Convert.ToInt32(r);
+            int sum = 0;
+            for (int i = 0; i < r; i++)
+            {
+                sum += p + i;
+            }
+            
+            Console.WriteLine(sum);
+            Console.ReadKey();
+        }
+    }
+}
