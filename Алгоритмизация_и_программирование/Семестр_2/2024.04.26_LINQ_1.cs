eec850c067054a044729e4eddd2414b38019c5c8
AnastasiiaEssina <145222942+AnastasiiaEssina@users.noreply.github.com>
2024-04-26 09:32:53 +0100

    Create 2024.04.26_LINQ_1.cs

---
+++ Алгоритмизация_и_программирование/Семестр_2/2024.04.26_LINQ_1.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Linq;
+using System.Collections.Generic;
+class HelloWorld {
+  static void Main() {
+    List<string> str = new List<string>() {"s", "jth", "98", "rtrt", "rtyu"};
+    var sel = from e in str where e.Length % 2 == 0 select e;
+    foreach (string s in sel){
+        Console.WriteLine(s);
+    }
+    Console.WriteLine("Введите количество строк, которые нужно добавить");
+    int a = int.Parse(Console.ReadLine());
+    for(int i = 0; i < a; i++)
+    {
+        str.Add(Console.ReadLine());
+    }
+
+    str = (from e in str where str.IndexOf(e)%2 != 0 select e).ToList();
+    sel = from e in str where e.Length % 2 == 0 select e;
+    Console.WriteLine("--------------");
+    foreach (string s in sel){
+        Console.WriteLine(s);
+    }
+  }
+}
